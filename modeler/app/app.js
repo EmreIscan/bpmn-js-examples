f05b9a73321d9f79f03efd216b2f5a736f5ef9d2
82isem1bwi <82isem1bwi@hft-stuttgart.de>
2023-01-12 21:09:21 +0100

    Funktion für CO2 Feld hinzugefügt. Verschiedene CO2 Werte nehmen verschiedene Farben an. Bis 25 wird es Grün, zwischen 25 und 75 wird es gelb und alles was darüber ist, wird rot.

--- modeler/app/app.js
+++ modeler/app/app.js
@@ -1,17 +1,143 @@
-import $ from 'jquery';
-
 import BpmnModeler from 'bpmn-js/lib/Modeler';
+
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
 
 import diagramXML from '../resources/newDiagram.bpmn';
 
-var container = $('#js-drop-zone');
+import customModule from './custom';
 
-var modeler = new BpmnModeler({
-  container: '#js-canvas',
-  keyboard: {
-    bindTo: window
+import qaExtension from '../resources/qa';
+
+const HIGH_PRIORITY = 1500;
+
+const containerEl = document.getElementById('container'),
+      qualityAssuranceEl = document.getElementById('quality-assurance'),
+      CO2ScoreEl = document.getElementById('CO2-score'),
+      lastCheckedEl = document.getElementById('last-checked'),
+      okayEl = document.getElementById('okay'),
+      formEl = document.getElementById('form'),
+      warningEl = document.getElementById('warning');
+
+window.addEventListener('click', (event) => {
+  const { target } = event;
+
+  if (target === qualityAssuranceEl || qualityAssuranceEl.contains(target)) {
+    return;
+  }
+
+  qualityAssuranceEl.classList.add('hidden');
+});
+
+const bpmnModeler = new BpmnModeler({
+  container: containerEl,
+  additionalModules: [
+    customModule
+  ],
+  moddleExtensions: {
+    qa: qaExtension
   }
 });
+
+bpmnModeler.importXML(diagramXML).then(() => {
+
+  const moddle = bpmnModeler.get('moddle'),
+        modeling = bpmnModeler.get('modeling');
+
+  let analysisDetails,
+      businessObject,
+      element,
+      CO2Score;
+
+
+  function validate() {
+    const { value } = CO2ScoreEl;
+
+    if (isNaN(value)) {
+      warningEl.classList.remove('hidden');
+      okayEl.disabled = true;
+    } else {
+      warningEl.classList.add('hidden');
+      okayEl.disabled = false;
+    }
+  }
+
+  bpmnModeler.on('element.contextmenu', HIGH_PRIORITY, (event) => {
+    event.originalEvent.preventDefault();
+    event.originalEvent.stopPropagation();
+
+    qualityAssuranceEl.classList.remove('hidden');
+
+    ({ element } = event);
+
+    // ignore root element
+    if (!element.parent) {
+      return;
+    }
+
+    businessObject = getBusinessObject(element);
+
+    let { suitable } = businessObject;
+
+    CO2ScoreEl.value = suitable ? suitable : '';
+
+    CO2ScoreEl.focus();
+
+    analysisDetails = getExtensionElement(businessObject, 'qa:AnalysisDetails');
+
+    lastCheckedEl.textContent = analysisDetails ? analysisDetails.lastChecked : '-';
+
+    validate();
+  });
+
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    CO2Score = Number(CO2ScoreEl.value);
+
+    if (isNaN(CO2Score)) {
+      return;
+    }
+
+    const extensionElements = businessObject.extensionElements || moddle.create('bpmn:ExtensionElements');
+
+    if (!analysisDetails) {
+      analysisDetails = moddle.create('qa:AnalysisDetails');
+
+      extensionElements.get('values').push(analysisDetails);
+    }
+
+    analysisDetails.lastChecked = new Date().toISOString();
+
+    modeling.updateProperties(element, {
+      extensionElements,
+      suitable: CO2Score
+    });
+
+    qualityAssuranceEl.classList.add('hidden');
+  });
+
+  formEl.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      qualityAssuranceEl.classList.add('hidden');
+    }
+  });
+
+  CO2ScoreEl.addEventListener('input', validate);
+
+}).catch((err) => {
+  console.error(err);
+});
+
+function getExtensionElement(element, type) {
+  if (!element.extensionElements) {
+    return;
+  }
+
+  return element.extensionElements.values.filter((extensionElement) => {
+    return extensionElement.$instanceOf(type);
+  })[0];
+}
 
 function createNewDiagram() {
   openDiagram(diagramXML);
